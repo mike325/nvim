9c9463c18ada1c95e24f31901ede7e5d9ddf91a0
Mike <mike@prodeveloper.me>
2017-10-31 18:27:28 -0700

    Extend shell skeleton

--- .resources/skeletons/skeleton.sh
+++ .resources/skeletons/skeleton.sh
@@ -20,3 +20,96 @@
 #             `+sso+:-`                 `.-/+oso:
 #            `++:.                           `-/+/
 #            .`                                 `/
+
+_NAME="$0"
+_NAME="${_NAME##*/}"
+
+_SCRIPT_PATH="$0"
+
+_SCRIPT_PATH="${_SCRIPT_PATH%/*}"
+
+if hash realpath 2>/dev/null; then
+    _SCRIPT_PATH=$(realpath "$_SCRIPT_PATH")
+else
+    pushd "$_SCRIPT_PATH" > /dev/null
+    _SCRIPT_PATH="$(pwd -P)"
+    popd > /dev/null
+fi
+
+# _DEFAULT_SHELL="${SHELL##*/}"
+_CURRENT_SHELL="bash"
+_IS_WINDOWS=0
+
+# Windows stuff
+if [[ $(uname --all) =~ MINGW ]]; then
+    _CURRENT_SHELL="$(ps | grep `echo $$` | awk '{ print $8 }')"
+    _CURRENT_SHELL="${_CURRENT_SHELL##*/}"
+    # Windows does not support links we will use cp instead
+    _IS_WINDOWS=1
+else
+    _CURRENT_SHELL="$(ps | head -2 | tail -n 1 | awk '{ print $4 }')"
+    # Hack when using sudo
+    # TODO: Must fix this
+    if [[ $_CURRENT_SHELL == "sudo" ]] || [[ $_CURRENT_SHELL == "su" ]]; then
+        _CURRENT_SHELL="$(ps | head -4 | tail -n 1 | awk '{ print $4 }')"
+    fi
+fi
+
+function help_user() {
+    echo ""
+    echo "  Description"
+    echo ""
+    echo "  Usage:"
+    echo "      $_NAME [OPTIONAL]"
+    echo ""
+    echo "      Optional Flags"
+    echo ""
+    echo "          -h, --help"
+    echo "              Display help, if you are seeing this, that means that you already know it (nice)"
+    echo ""
+}
+
+function __parse_args() {
+    local arg="$1"
+    local name="$2"
+
+    local pattern="^--$name[=][a-zA-Z0-9./]+$"
+    if [[ ! -z "$3" ]]; then
+        local pattern="^--$name[=]$3$"
+    fi
+
+    if [[ $arg =~ $pattern ]]; then
+        local left_side="${arg#*=}"
+        echo "$left_side"
+    else
+        echo "$arg"
+    fi
+}
+
+function warn_msg() {
+    WARN_MESSAGE="$1"
+    printf "[!]     ---- Warning!!! %s \n" "$WARN_MESSAGE"
+}
+
+function error_msg() {
+    ERROR_MESSAGE="$1"
+    printf "[X]     ---- Error!!!   %s \n" "$ERROR_MESSAGE" 1>&2
+}
+
+function status_msg() {
+    STATUS_MESSAGGE="$1"
+    printf "[*]     ---- %s \n" "$STATUS_MESSAGGE"
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -h|--help)
+            help_user
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+
