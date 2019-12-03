c9dcadefbb0cb0b2eb0b4b8952063ba3781831c3
Mike <mickiller.25@gmail.com>
2019-12-02 23:23:41 -0600

    revert: Revert ":" syntax for nvim module

--- lua/nvim.lua
+++ lua/nvim.lua
@@ -5,37 +5,37 @@ local function nvimFuncWrapper (name, ...)
     return vim.api.nvim_call_function(name, {...})
 end
 
-function nvim:has(feature) return nvimFuncWrapper('has', feature) end
-function nvim:join(str, separator) return nvimFuncWrapper('join', str, separator) end
-function nvim:split(str, pattern, keepempty) return nvimFuncWrapper('split', str, pattern, keepempty) end
-function nvim:system(cmd) return nvimFuncWrapper('system', cmd) end
-function nvim:exists(setting) return nvimFuncWrapper('exists', setting) end
-function nvim:getcwd() return nvimFuncWrapper('getcwd') end
-function nvim:stdpath(path) return nvimFuncWrapper('stdpath', path) end
-function nvim:exepath(program) return nvimFuncWrapper('exepath', program) end
-function nvim:executable(program) return nvimFuncWrapper('executable', program) end
-function nvim:systemlist(cmd) return nvimFuncWrapper('systemlist', cmd) end
+function nvim.has(feature) return nvimFuncWrapper('has', feature) end
+function nvim.join(str, separator) return nvimFuncWrapper('join', str, separator) end
+function nvim.split(str, pattern, keepempty) return nvimFuncWrapper('split', str, pattern, keepempty) end
+function nvim.system(cmd) return nvimFuncWrapper('system', cmd) end
+function nvim.exists(setting) return nvimFuncWrapper('exists', setting) end
+function nvim.getcwd() return nvimFuncWrapper('getcwd') end
+function nvim.stdpath(path) return nvimFuncWrapper('stdpath', path) end
+function nvim.exepath(program) return nvimFuncWrapper('exepath', program) end
+function nvim.executable(program) return nvimFuncWrapper('executable', program) end
+function nvim.systemlist(cmd) return nvimFuncWrapper('systemlist', cmd) end
 
-function nvim:mkdir(dir, ...) return nvimFuncWrapper('mkdir', dir, ...) end
-function nvim:isdirectory(dir) return nvimFuncWrapper('isdirectory', dir) end
-function nvim:filereadable(file) return nvimFuncWrapper('filereadable', file) end
-function nvim:filewritable(file) return nvimFuncWrapper('filewritable', file) end
+function nvim.mkdir(dir, ...) return nvimFuncWrapper('mkdir', dir, ...) end
+function nvim.isdirectory(dir) return nvimFuncWrapper('isdirectory', dir) end
+function nvim.filereadable(file) return nvimFuncWrapper('filereadable', file) end
+function nvim.filewritable(file) return nvimFuncWrapper('filewritable', file) end
 
-function nvim:has_version(version) return vim.api.nvim_call_function('has', {'nvim-'..version}) end
+function nvim.has_version(version) return vim.api.nvim_call_function('has', {'nvim-'..version}) end
 
-function nvim:realpath(path)
+function nvim.realpath(path)
     path = path == '.' and getcwd() or path
     return nvimFuncWrapper('fnamemodify', path, ':p')
 end
 
-function nvim:globpath(path, expr)
+function nvim.globpath(path, expr)
     path = path == '.' and getcwd() or path
     local nosuf = false
     local list = true
     return nvimFuncWrapper('globpath', path, expr, nosuf, list)
 end
 
-function nvim:finddir(name, path, ...)
+function nvim.finddir(name, path, ...)
     path = path == '.' and getcwd() or path
     local count = {...}
     if #count > 0 then
@@ -44,7 +44,7 @@ function nvim:finddir(name, path, ...)
     return nvimFuncWrapper('finddir', name, path)
 end
 
-function nvim:findfile(name, path, ...)
+function nvim.findfile(name, path, ...)
     path = path == '.' and getcwd() or path
     local count = {...}
     if #count > 0 then
@@ -61,8 +61,8 @@ function nvim.json.encode(json) return nvimFuncWrapper('json_encode', json) end
 -- nvim.json.read   = function(json) return nvimFuncWrapper('json_encode', json) end
 -- nvim.json.write  = function(json) return nvimFuncWrapper('json_encode', json) end
 
-function nvim:set_env(env, value) vim.loop.os_setenv(env, value) end
-function nvim:get_env(env)
+function nvim.set_env(env, value) vim.loop.os_setenv(env, value) end
+function nvim.get_env(env)
     local value = vim.loop.os_getenv(env)
     return value ~= nil and value or nil
 end
