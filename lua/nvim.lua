f6185e5e2168e894aef8bd3bab72259ac9abd21c
Mike <mickiller.25@gmail.com>
2019-12-03 21:01:01 -0600

    feat: add lua mapping functions
    New lua functions to work with mappings

--- lua/nvim.lua
+++ lua/nvim.lua
@@ -2,10 +2,78 @@
 local nvim = {}
 local api = vim.api
 
+local function nvim_get_mapping(m, lhs, ...)
+    local mappings
+    local mapping
+
+    local opts = ...
+
+    local modes = {
+        normal = 'n',
+        insert = 'i',
+        visual = 'v',
+        select  = 's',
+        command = 'c',
+        terminal = 't',
+    }
+
+    local mode = modes[m] ~= nil and modes[m] or m
+
+    if opts['buffer'] ~= nil and opts['buffer'] == true then
+        mappings = vim.api.nvim_buf_get_keymap(mode)
+    else
+        mappings = vim.api.nvim_get_keymap(mode)
+    end
+
+    for _,map in pairs(mappings) do
+        if map['lhs'] == lhs then
+            mapping = map['rhs']
+            break
+        end
+    end
+
+    return mapping
+
+end
+
+local function nvim_set_mapping(m, lhs, rhs, ...)
+    local opts = ...
+
+    local modes = {
+        normal = 'n',
+        insert = 'i',
+        visual = 'v',
+        select  = 's',
+        command = 'c',
+        terminal = 't',
+    }
+
+    local mode = modes[m] ~= nil and modes[m] or m
+
+    if opts['buffer'] ~= nil and opts['buffer'] == true then
+        opts['buffer'] = nil
+
+        if rhs ~= nil then
+            vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, opts)
+        else
+            vim.api.nvim_buf_del_keymap(0, mode, lhs)
+        end
+    else
+        if rhs ~= nil then
+            vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+        else
+            vim.api.nvim_del_keymap(mode, lhs)
+        end
+    end
+
+end
+
 -- Took from https://github.com/norcalli/nvim_utils
 -- GPL3 apply to the nvim object
 nvim = setmetatable({
     l = api.loop;
+    nvim_get_mapping = nvim_get_mapping;
+    nvim_set_mapping = nvim_set_mapping;
     fn = setmetatable({}, {
         __index = function(self, k)
         local mt = getmetatable(self)
