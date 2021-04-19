45ae39ddcbedf3b2157fb1a4de8fa5a67325d699
Mike <mickiller.25@gmail.com>
2021-04-19 12:14:35 -0500

    feat: Add new echowarn function
    Use nvim_echo API to create echowarn function

--- lua/tools/messages.lua
+++ lua/tools/messages.lua
@@ -1,9 +1,13 @@
 local M = {}
 
 function M.echoerr(msg)
-    if type(msg) == 'string' and #msg > 0 then
-        vim.api.nvim_err_writeln(msg)
-    end
+    assert(type(msg) == 'string' and #msg > 0, 'Invalid message: '..vim.inspect(msg))
+    vim.api.nvim_err_writeln(msg)
+end
+
+function M.echowarn(msg)
+    assert(type(msg) == 'string' and #msg > 0, 'Invalid message: '..vim.inspect(msg))
+    vim.api.nvim_echo({{msg, 'WarningMsg'}}, true, {})
 end
 
 return M
