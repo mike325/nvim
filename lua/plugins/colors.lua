f82a28f01e211f4b24ec47f6149fd361288128bb
Mike <mickiller.25@gmail.com>
2022-03-24 14:18:23 -0600

    feat: Add more colors to the named colors dictionary

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -11,19 +11,22 @@ end
 local colors = {
     white        = '#FFFFFF',
     black        = '#000000',
+    red          = '#EF0F00',
     cyan         = '#008080',
     blue         = '#0000D8',
     purple       = '#5D4D7A',
     pink         = '#D16D9E',
     orange       = '#FF8800',
     brown        = '#825A03',
+    yellow       = '#FAF000',
+    green        = '#00F800',
     light_yellow = '#FAFF00',
     dark_yellow  = '#E0DF3F',
     light_green  = '#00C800',
     dark_green   = '#004800',
     light_gray   = '#404040',
     dark_gray    = '#282828',
-    light_red    = '#E80000',
+    light_red    = '#F00000',
     dark_red     = '#480000',
 }
 
