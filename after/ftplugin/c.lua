d2627c1022229871e56d60f8aa8eda1262539d0b
Mike <mickiller.25@gmail.com>
2022-08-29 18:46:25 +0200

    fix: Remove in C/C++ includeexpr config option

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,5 +1,4 @@
 require('utils.buffers').setup('cpp', {
     define = [[^\(#\s*define\|[a-z]*\s*const\(expr\)\?\s*[a-z]*\)]],
-    includeexpr = [[substitute(v:fname,'\.','/','g')]],
     commentstring = '// %s',
 })
--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,6 +1,5 @@
 local ft = vim.opt_local.filetype:get()
 require('utils.buffers').setup(ft, {
     define = [[^\(#\s*define\|[a-z]*\s*const\(expr\)\?\s*[a-z]*\)]],
-    includeexpr = [[substitute(v:fname,'\.','/','g')]],
     commentstring = '// %s',
 })
