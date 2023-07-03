8b67eeac1e11b1c40405e6c04fb0e2bc1fce36ee
Mike <mickiller.25@gmail.com>
2023-07-03 10:39:35 +0200

    fix: Small improvements in project config and cleanup
    Use vim.secure to source project files
    Remove dead code
    Enable exrc
    Add new autocmd to cleanup help files
    Remove wipe from help bufhidden opts

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -2,7 +2,7 @@ vim.opt_local.number = true
 vim.opt_local.relativenumber = true
 vim.opt_local.buflisted = true
 vim.opt_local.list = false
-vim.opt_local.bufhidden = 'wipe'
+-- vim.opt_local.bufhidden = 'wipe'
 
 vim.keymap.set('n', 'q', '<cmd>q!<CR>', { noremap = true, silent = true, nowait = true, buffer = true })
 vim.keymap.set('n', '<CR>', '<C-]>', { noremap = true, silent = true, nowait = true, buffer = true })
