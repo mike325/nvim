f54585de017b34d24a2f79cd50ce5f1e30cdc3ab
Mike <mickiller.25@gmail.com>
2024-07-06 14:49:08 +0200

    fix: update badges

--- test/test.ps1
+++ test/test.ps1
@@ -12,5 +12,5 @@ if ( -Not (Test-Path("$env:USERPROFILE/vimfiles")) ) {
     git clone --branch=vim --recursive "https://github.com/mike325/nvim.git" "$env:USERPROFILE/vimfiles"
 }
 
-vim -N --cmd 'let g:bare=1' -c 'autocmd VimEnter * qa!'
+vim -N --cmd 'let g:bare=1' --cmd version -Es -V2 -c 'autocmd VimEnter * qa!'
 exit $LASTEXITCODE
