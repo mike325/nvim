988730f2ec6bcd1f566df0431fab12afc8bbbb1a
Autoformatter <autoformatter@users.noreply.github.com>
2025-05-28 12:06:26 +0000

    :art: Committing stylua and shfmt changes

--- after/ftplugin/robot.lua
+++ after/ftplugin/robot.lua
@@ -5,7 +5,7 @@ local cwd = vim.pesc(vim.uv.cwd() .. '/')
 local basedir = vim.fs.dirname(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
 
 basedir = (basedir:gsub(cwd, ''))
-basedir = basedir:gsub(vim.fs.basename(basedir)..'$', 'resources')
+basedir = basedir:gsub(vim.fs.basename(basedir) .. '$', 'resources')
 vim.opt_local.path:append(basedir)
 
 vim.wo.spell = false
