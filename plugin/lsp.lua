11d5c4afdd377e0764d1eaf95f6a93ec47d15fdd
Mike <mickiller.25@gmail.com>
2025-06-10 16:32:16 +0200

    fix: improve lsp config loading

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -4,8 +4,7 @@ if not has_lsp_config then
     lsp_enable_group = vim.api.nvim_create_augroup('LspEnableConfig', { clear = true })
 end
 
-local lsp_configs = 'after/lsp'
-
+-- NOTE: Python server usage priority
 local python_servers = {
     'pylsp',
     'pylyzer',
@@ -15,27 +14,29 @@ local python_servers = {
 
 -- These are the configs I'm intersted in, they can be defined with a empty dict
 -- and resolved with vim.lsp.config
-local configs = vim.iter(vim.api.nvim_get_runtime_file(('%s/*.lua'):format(lsp_configs), true))
-    :map(function(config)
-        return (vim.fs.basename(config):gsub('%.lua$', ''))
-    end)
-    :totable()
+local configs = vim.api.nvim_get_runtime_file('after/lsp/*.lua', true)
 
 --- Check if the server is available
----@param config_name string
+---@param configname string
+---@return string?
+local function find_config(configname)
+    return vim.iter(configs):find(function(c)
+        return (vim.fs.basename(c):gsub('%.lua$', '')) == configname
+    end)
+end
+
+--- Check if the server is available
+---@param configname string
 ---@return boolean
-local function has_server(config_name)
+local function has_server(configname)
     local config
-    if not has_lsp_config then
-        local configfile = vim.iter(vim.api.nvim_get_runtime_file(('%s/*.lua'):format(lsp_configs), true))
-            :find(function(c)
-                return (vim.fs.basename(c):gsub('%.lua$', '')) == config_name
-            end)
+    if has_lsp_config then
+        config = vim.lsp.config[configname]
+    else
+        local configfile = find_config(configname)
         if configfile then
             config = dofile(configfile)
         end
-    else
-        config = vim.lsp.config[config_name]
     end
     if config and config.cmd then
         return vim.fn.executable(config.cmd[1]) == 1
@@ -44,45 +45,43 @@ local function has_server(config_name)
 end
 
 --- Enable lsp servers
----@param servers string[]|string
-local function enable_server(servers)
+---@param server string
+local function enable_server(server)
     if has_lsp_config then
-        vim.lsp.enable(servers)
+        vim.lsp.enable(server)
     else
-        servers = type(servers) == type {} and servers or { servers }
-        vim.iter(configs):each(function(configfile)
-            local configname = (vim.fs.basename(configfile):gsub('%.lua$', ''))
-            if vim.list_contains(servers, configname) then
-                local config = dofile(configfile)
-                local fts = config.filetypes
+        local configfile = find_config(server)
+        if configfile then
+            local config = dofile(configfile)
+            local fts = config.filetypes
 
-                vim.api.nvim_create_autocmd({ 'Filetype' }, {
-                    desc = 'Enable LSP config on filetypes',
-                    group = lsp_enable_group,
-                    pattern = table.concat(fts, ','),
-                    callback = function(args)
-                        local root_dir = vim.uv.cwd()
-                        if config.root_markers or config.root_dir then
-                            root_dir = vim.fs.root(args.buf, config.root_markers or config.root_dir)
-                        end
-                        config.name = configname
-                        config.root_dir = root_dir
-                        vim.lsp.start(config, { bufnr = args.buf })
-                    end,
-                })
-            end
-        end)
+            vim.api.nvim_create_autocmd({ 'Filetype' }, {
+                desc = 'Enable LSP config on filetypes',
+                group = lsp_enable_group,
+                pattern = table.concat(fts, ','),
+                callback = function(args)
+                    local root_dir = vim.uv.cwd()
+                    if config.root_markers or config.root_dir then
+                        root_dir = vim.fs.root(args.buf, config.root_markers or config.root_dir)
+                    end
+                    config.name = server
+                    config.root_dir = root_dir
+                    vim.lsp.start(config, { bufnr = args.buf })
+                end,
+            })
+        end
     end
 end
 
-local servers = vim.iter(configs)
+local confignames = vim.iter(configs):map(vim.fs.basename):map(require('utils.files').filename):totable()
+
+vim.iter(confignames)
     :filter(function(config)
         return not vim.list_contains(python_servers, config) and has_server(config)
     end)
-    :totable()
-enable_server(servers)
+    :each(enable_server)
 
-local python_configs = vim.iter(configs)
+local python_configs = vim.iter(confignames)
     :filter(function(config)
         return vim.list_contains(python_servers, config) and has_server(config)
     end)
