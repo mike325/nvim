7320fd54bcbe0c0548173fbfaf8a56ca002e5084
Autoformatter <autoformatter@users.noreply.github.com>
2025-06-27 10:30:41 +0000

    :art: Committing stylua and shfmt changes

--- after/compiler/stylua.lua
+++ after/compiler/stylua.lua
@@ -1,5 +1,5 @@
 local name = 'stylua'
-local compiler = RELOAD('utils.functions').get_compiler(name, {language = 'lua'})
+local compiler = RELOAD('utils.functions').get_compiler(name, { language = 'lua' })
 
 vim.cmd.CompilerSet('makeprg=' .. compiler.makeprg)
 if compiler.efm then
