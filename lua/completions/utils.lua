846420d4e732bbdc4211a606b51f1c4ef14f5498
Mike <mickiller.25@gmail.com>
2025-07-16 16:21:54 +0200

    feat: Add new completion generator function

--- lua/completions/utils.lua
+++ lua/completions/utils.lua
@@ -41,6 +41,82 @@ function M.general_nodash_completion(arglead, _, _, options)
     return filter(arglead:lower(), options)
 end
 
+--- Return a completion function
+---@param cmd string[]
+---@param options string[]
+---@return string[]
+local function get_available_flags(cmd, options)
+    return vim.iter(options)
+        :filter(function(opt)
+            return not vim.tbl_contains(cmd, opt)
+        end)
+        :totable()
+end
+
+--- Return a completion function
+---@param comp_func fun(arglead: string, cmdline: string, cursorpos: string, options: string[]):string[]
+---@param options string[]|fun(cmd: string[]?):string[]
+---@param suboptions boolean|table<string, string[]|fun(cmd: string[]?):string[]>|nil
+---@param smart boolean?
+local function get_completions_func(comp_func, options, suboptions, smart)
+    if type(suboptions) == type(true) then
+        smart = suboptions --[[@as boolean]]
+        suboptions = nil
+    end
+
+    return function(arglead, cmdline, cursorpos)
+        local cmd = M.get_cmd(cmdline)
+
+        if vim.is_callable(options) then
+            options = options(cmd)
+        end
+
+        if smart then
+            options = get_available_flags(cmd, options --[[@as string[] ]])
+        end
+
+        if suboptions then
+            for flag, subopts in
+                pairs(suboptions --[[@as table<string, string[]|fun(cmd: string[]?):string[]>]])
+            do
+                local pattern = string.format('^%%-?%%-?%s$', flag)
+                if
+                    arglead:match(pattern)
+                    or (arglead == '' and cmd[#cmd]:match(pattern))
+                    or (arglead ~= '' and not arglead:match '^%-' and #cmd >= 2 and cmd[#cmd - 1]:match(pattern))
+                then
+                    if vim.is_callable(subopts) then
+                        subopts = subopts(cmd)
+                    end
+
+                    if smart then
+                        subopts = get_available_flags(cmd, subopts --[[@as string[] ]])
+                    end
+                    return comp_func(arglead, cmdline, cursorpos, subopts --[[@as string[] ]])
+                end
+            end
+        end
+
+        return comp_func(arglead, cmdline, cursorpos, options --[[@as string[] ]])
+    end
+end
+
+--- Return a completion function
+---@param options string[]|fun(cmd: string[]?):string[]
+---@param suboptions boolean|table<string, string[]|fun(cmd: string[]?):string[]>|nil
+---@param smart boolean?
+function M.get_completion(options, suboptions, smart)
+    return get_completions_func(M.general_completion, options, suboptions, smart)
+end
+
+--- Return a completion function
+---@param options string[]|fun(cmd: string[]?):string[]
+---@param suboptions boolean|table<string, string[]|fun(cmd: string[]?):string[]>|nil
+---@param smart boolean?
+function M.get_nodash_completion(options, suboptions, smart)
+    return get_completions_func(M.general_nodash_completion, options, suboptions, smart)
+end
+
 function M.json_keys_completion(arglead, cmdline, cursorpos, filename, funcs)
     funcs = funcs or {}
 
