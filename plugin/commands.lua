f5e89248e77087473fc3a36f103e5b0a53ad8b95
Autoformatter <autoformatter@users.noreply.github.com>
2025-12-08 12:51:26 +0000

    :art: Committing stylua and shfmt changes

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -923,7 +923,7 @@ then
     nvim.command.set('OpenUML', function()
         local bufname = vim.api.nvim_buf_get_name(0)
         local basename = vim.fs.basename(bufname)
-        if basename:match('%.puml') then
+        if basename:match '%.puml' then
             local dir = vim.fs.dirname(bufname)
             local img = vim.fs.joinpath(dir, (basename:gsub('%.puml', '.png')))
             vim.ui.open(img)
